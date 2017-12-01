bf1a4d7bf98f3bb305920c4300c174c01a0383e0
gormanjp <jpgorman8@gmail.com>
2017-12-01 14:09:32 -0500

    add resume modal

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import {
   HashRouter as Router,
   Route,
@@ -6,22 +7,56 @@ import {
 } from 'react-router-dom';
 
 export class Nav extends React.Component {
-  super(){
+  constructor(){
+    super();
+    this.state = {
+      modalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.close = this.close.bind(this);
+  }
 
+  toggleModal() {
+    this.setState({
+      modalOpen: !this.state.modalOpen
+    })
+  }
+
+  close() {
+    this.setState({
+      modalOpen: false
+    })
   }
   render(){
     return (
-    <Router hashType="noslash">
-      <div className="header raleway ralewayBig">
-        <div className="container">
-          <ul className="nav navbar-nav">
-            <li><Link to="home"><span className="glyphicon glyphicon-chevron-up" aria-hidden="true"></span></Link></li>
-            <li><Link to="about" className="nav__hover">ABOUT</Link></li>
-            <li><Link to="projects" className="nav__hover">PROJECTS</Link></li>
-            <li><Link to="home" className="nav__hover">RESUME</Link></li>
-          </ul>
+    <div className="">
+      <Modal bsSize="large" style={{marginTop: '40px'}} show={this.state.modalOpen} onHide={this.close}>
+        <Modal.Body>
+          <img
+            src="resume.png"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%"
+            }}
+            ></img>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+      <Router hashType="noslash">
+        <div className="header raleway ralewayBig">
+          <div className="container">
+            <ul className="nav navbar-nav">
+              <li><Link to="home"><span className="glyphicon glyphicon-chevron-up" aria-hidden="true"></span></Link></li>
+              <li><Link to="about" className="nav__hover">ABOUT</Link></li>
+              <li><Link to="projects" className="nav__hover">PROJECTS</Link></li>
+              <li><Link to="home" className="nav__hover" onClick={this.toggleModal}>RESUME</Link></li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </Router>)
+      </Router>
+    </div>
+    )
   }
 }
